1cbd086704077e54fe0207b76001df03e6536f66
Hugo Platzer <hplatzer@cosy.sbg.ac.at>
2016-10-13 13:59:52 +0200

    added tsv parser

---
+++ a1/code/TSVParser.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class TSVParser {
+	private BufferedReader br;
+	
+    public TSVParser(String filename) throws IOException {
+    	br = new BufferedReader(new FileReader(filename));
+    }
+    
+    public String[] parseLine() throws IOException {
+    	List<String> values = new ArrayList<>();
+    	String line = br.readLine();
+    	return line.split("\\t");
+    }
+    
+    public void close() throws IOException {
+    	br.close();
+    }
+}
