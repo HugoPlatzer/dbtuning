056fc398d6bc89f218c9fb515214553321df49f3
Hugo Platzer <hplatzer@cosy.sbg.ac.at>
2016-10-13 14:50:07 +0200

    code cleanup

--- a1/code/DBConnect.java
+++ a1/code/DBConnect.java
@@ -3,83 +3,75 @@ import java.sql.*;
 
 public class DBConnect {
 
-    public static void printResultSetRow(ResultSet rs) throws SQLException {
-        ResultSetMetaData rsmd = rs.getMetaData();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-            if (i > 1) {
-                sb.append(", ");
-            }
-            sb.append(rsmd.getColumnName(i) + " = " + rs.getString(i));
-        }
-        System.out.println(sb);
-    }
+	public static void printResultSetRow(ResultSet rs) throws SQLException {
+		ResultSetMetaData rsmd = rs.getMetaData();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+			if (i > 1) {
+				sb.append(", ");
+			}
+			sb.append(rsmd.getColumnName(i) + " = " + rs.getString(i));
+		}
+		System.out.println(sb);
+	}
 
-    public static void printResultSet(ResultSet rs) throws SQLException {
-        while (rs.next()) {
-            printResultSetRow(rs);
-        }
-    }
+	public static void printResultSet(ResultSet rs) throws SQLException {
+		while (rs.next()) {
+			printResultSetRow(rs);
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        String host = "biber.cosy.sbg.ac.at";
-        String port = "5432";
-        String database = "dbtuning_ws2016";
-        String user = "hplatzer";
-        String pwd = "Aicae4paed4e";
-        String url = "jdbc:postgresql://" + host + ":" + port + "/" + database;
-        String query = "INSERT INTO auth (pubid, name) values (?, ?)";
-        
-        //Normales Insert
-        try (Connection con = DriverManager.getConnection(url, user, pwd);){
-                PreparedStatement stmt = con.prepareStatement(query);
-        		TSVParser parser = new TSVParser("auth.tsv");
-        		long startTime = System.nanoTime();
-    		
-        		for(int i = 0; i < 10000; i++){
-        			String[] data = parser.parseLine();
-        		
-        			stmt.setInt(1, Integer.parseInt(data[0]));
-        			stmt.setString(2, data[1]);
-	            	stmt.executeUpdate();
-        		}
-        	
-        		System.out.println((System.nanoTime() - startTime) / 1000000 + "ms");
-        		parser.close();
-        		stmt.close();
-        		con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
-        //Batch Insert
-        try (Connection con = DriverManager.getConnection(url, user, pwd);){
-        	PreparedStatement stmt = con.prepareStatement(query);
-        	final int batchSize = 1000;
-        	int count = 0;
-        	TSVParser parser = new TSVParser("auth.tsv");
+	public static void insertSimple(Connection con, TSVParser parser, int n) throws SQLException, IOException {
+		String query = "INSERT INTO auth (name, pubid) values (?, ?)";
+		PreparedStatement stmt = con.prepareStatement(query);
+		for (int i = 0; i < n; i++) {
+			String[] data = parser.parseLine();
+			stmt.setString(1, data[0]);
+			stmt.setString(2, data[1]);
+			stmt.executeUpdate();
+		}
+		stmt.close();
+	}
 
-        	long startTime = System.nanoTime();
-        	for(int i = 0; i < 10000; i++){
-        		String[] data = parser.parseLine();
-        	
-	            stmt.setInt(1, Integer.parseInt(data[0]));
-	            stmt.setString(2, data[1]);
-	            stmt.addBatch();
-	            
-	            if(++count % batchSize == 0){
-	            	stmt.executeBatch();
-	            	count = 0;
-	            }
-        	}
+	public static void insertBatch(Connection con, TSVParser parser, int n, int batchSize)
+			throws SQLException, IOException {
+		String query = "INSERT INTO auth (name, pubid) values (?, ?)";
+		PreparedStatement stmt = con.prepareStatement(query);
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			String[] data = parser.parseLine();
+			stmt.setString(1, data[0]);
+			stmt.setString(2, data[1]);
+			stmt.addBatch();
 
-        	System.out.println((System.nanoTime() - startTime) / 1000000 + "ms");
-	        parser.close();
-	        stmt.executeBatch();
-	       	stmt.close();
-	       	con.close();
-       } catch (SQLException e) {
-           e.printStackTrace();
-       }
-    }
+			if (++count % batchSize == 0) {
+				stmt.executeBatch();
+				count = 0;
+			}
+		}
+		stmt.executeBatch();
+		stmt.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		String host = "biber.cosy.sbg.ac.at";
+		String port = "5432";
+		String database = "dbtuning_ws2016";
+		String user = "hplatzer";
+		String pwd = "Aicae4paed4e";
+		String url = "jdbc:postgresql://" + host + ":" + port + "/" + database;
+
+		try (Connection con = DriverManager.getConnection(url, user, pwd);) {
+
+			TSVParser parser = new TSVParser("../../data/auth.tsv");
+			long startTime = System.nanoTime();
+			
+			insertBatch(con, parser, 100, 10);
+			
+			System.out.println((System.nanoTime() - startTime) / 1000000 + "ms");
+			parser.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
