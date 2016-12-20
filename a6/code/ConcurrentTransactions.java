0b262de1a360bc376ec11984d12c748c64e2c805
Raydercorp <mario.strohmeier2@gmail.com>
2016-12-20 14:08:43 +0100

    fill table

--- a6/code/ConcurrentTransactions.java
+++ a6/code/ConcurrentTransactions.java
@@ -11,6 +11,7 @@ package account;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.sql.*;
 
 /** 
  * Dummy transaction that prints a start message, waits for a random time 
@@ -20,20 +21,19 @@ class Transaction extends Thread {
 
 	// identifier of the transaction
 	int id;
+	Connection con;
 	
-	Transaction(int id) {
+	Transaction(int id, Connection con) {
 		this.id = id;
+		this.con = con;
 	}
 	
 	@Override
 	public void run() {
 		System.out.println("transaction " + id + " started");
 		
-		// replace this with a transaction
-		int ms = (int)(Math.random()*100);
-		try {
-			sleep(ms);
-		} catch (Exception e) {};
+		String query = "declare @e varchar, @c varchar; SELECT balance FROM Accounts WHERE account=?;
+		PreparedStatement stmt = con.prepareStatement(query);
 		
 		System.out.println("transaction " + id + " terminated");
 	}	
@@ -51,6 +51,12 @@ class Transaction extends Thread {
 public class ConcurrentTransactions {
 
 	public static void main(String[] args) {
+		String host = "biber.cosy.sbg.ac.at";
+		String port = "5432";
+		String database = "dbtuning_ws2016";
+		String user = "hplatzer";
+		String pwd = "Aicae4paed4e";
+		String url = "jdbc:postgresql://" + host + ":" + port + "/" + database;
 
 		// read command line parameters
 		if (args.length != 2) {
@@ -60,10 +66,14 @@ public class ConcurrentTransactions {
 		int numThreads = Integer.parseInt(args[0]);
 		int maxConcurrent = Integer.parseInt(args[1]);
 
-		// create numThreads transactions
-		Transaction[] trans = new Transaction[numThreads];
-		for (int i = 0; i < trans.length; i++) {
-			trans[i] = new Transaction(i + 1);
+		try (Connection con = DriverManager.getConnection(url, user, pwd);) {
+			// create numThreads transactions
+			Transaction[] trans = new Transaction[numThreads];
+			for (int i = 0; i < trans.length; i++) {
+				trans[i] = new Transaction(i + 1, con);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
 		// start all transactions using a thread pool 
