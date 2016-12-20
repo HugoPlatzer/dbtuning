428b2baf88d00e072b3ed8b9e28a8f6992fddbed
Raydercorp <mario.strohmeier2@gmail.com>
2016-12-20 16:03:19 +0100

    sadggsdvgbkl

--- a6/code/ConcurrentTransactions.java
+++ a6/code/ConcurrentTransactions.java
@@ -19,62 +19,71 @@ class Transaction extends Thread {
 
 	// identifier of the transaction
 	int id;
-	Connection con;
 	
-	Transaction(int id, Connection con) {
+	Transaction(int id) {
 		this.id = id;
-		this.con = con;
 	}
 	
 	@Override
 	public void run() {
+		String host = "biber.cosy.sbg.ac.at";
+		String port = "5432";
+		String database = "dbtuning_ws2016";
+		String user = "hplatzer";
+		String pwd = "Aicae4paed4e";
+		String url = "jdbc:postgresql://" + host + ":" + port + "/" + database;
 		int e = 0, c = 0;
 		
-		try {
-			System.out.println("transaction " + id + " started");
+		try (Connection con = DriverManager.getConnection(url, user, pwd);) {
+			try {
+				System.out.println("transaction " + id + " started");
 
-			con.setAutoCommit(false);
+				con.setAutoCommit(false);
 
-			String query = "SELECT balance FROM Accounts WHERE account=?";
-			PreparedStatement stmt = con.prepareStatement(query);
-			stmt.setInt(1, id);
-			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				e = rs.getInt(1);
+				String query = "SELECT balance FROM Accounts WHERE account=?";
+				PreparedStatement stmt = con.prepareStatement(query);
+				stmt.setInt(1, id);
+				ResultSet rs = stmt.executeQuery();
+				while (rs.next()) {
+					e = rs.getInt(1);
+				}
+
+				query = "UPDATE Accounts SET balance=? WHERE account=?";
+				stmt = con.prepareStatement(query);
+				stmt.setInt(1, e+1);
+				stmt.setInt(2, id);
+				stmt.executeUpdate();
+
+				query = "SELECT balance FROM Accounts WHERE account=?";
+				stmt = con.prepareStatement(query);
+				stmt.setInt(1, 0);
+				rs = stmt.executeQuery();
+				while (rs.next()) {
+					c = rs.getInt(1);
+				}
+
+				query = "UPDATE Accounts SET balance=? WHERE account=?";
+				stmt = con.prepareStatement(query);
+				stmt.setInt(1, c-1);
+				stmt.setInt(2, 0);
+				stmt.executeUpdate();
+
+				con.commit();
+
+				System.out.println("transaction " + id + " terminated");
 			}
-
-			query = "UPDATE Accounts SET balance=? WHERE account=?";
-			stmt = con.prepareStatement(query);
-			stmt.setInt(1, e+1);
-			stmt.setInt(2, id);
-			stmt.executeQuery();
-
-			query = "SELECT balance FROM Accounts WHERE account=?";
-			stmt = con.prepareStatement(query);
-			stmt.setInt(1, 0);
-			rs = stmt.executeQuery();
-			while (rs.next()) {
-				c = rs.getInt(1);
+			catch(SQLException err){
+				err.printStackTrace();
+				try{			
+					con.rollback();
+				}
+				catch(SQLException err2){
+					err2.printStackTrace();
+				}
 			}
-
-			query = "UPDATE Accounts SET balance=? WHERE account=?";
-			stmt = con.prepareStatement(query);
-			stmt.setInt(1, c-1);
-			stmt.setInt(2, 0);
-			stmt.executeQuery();
-
-			con.commit();
-
-			System.out.println("transaction " + id + " terminated");
 		}
-		catch(SQLException err){
-			System.out.println(err);
-			try{			
-				con.rollback();
-			}
-			catch(SQLException err2){
-				System.out.println(err2);
-			}
+		catch(SQLException err3){
+			err3.printStackTrace();
 		}
 	}	
 	
@@ -91,38 +100,26 @@ class Transaction extends Thread {
 public class ConcurrentTransactions {
 
 	public static void main(String[] args) {
-		String host = "biber.cosy.sbg.ac.at";
-		String port = "5432";
-		String database = "dbtuning_ws2016";
-		String user = "hplatzer";
-		String pwd = "Aicae4paed4e";
-		String url = "jdbc:postgresql://" + host + ":" + port + "/" + database;
-
 		// read command line parameters
 		if (args.length != 2) {
-			System.err.println("params: numThreads maxConcurrent");
+			System.out.println("params: numThreads maxConcurrent");
 			System.exit(-1);
 		}	
 		int numThreads = Integer.parseInt(args[0]);
 		int maxConcurrent = Integer.parseInt(args[1]);
 
-		try (Connection con = DriverManager.getConnection(url, user, pwd);) {
-			// create numThreads transactions
-			Transaction[] trans = new Transaction[numThreads];
-			for (int i = 0; i < trans.length; i++) {
-				trans[i] = new Transaction(i + 1, con);
-			}
-
-			// start all transactions using a thread pool 
-			ExecutorService pool = Executors.newFixedThreadPool(maxConcurrent);				
-			for (int i = 0; i < trans.length; i++) {
-				pool.execute(trans[i]);
-			}		
-			pool.shutdown(); // end program after all transactions are done	
-		} catch (SQLException e) {
-			e.printStackTrace();
+		// create numThreads transactions
+		Transaction[] trans = new Transaction[numThreads];
+		for (int i = 0; i < trans.length; i++) {
+			trans[i] = new Transaction(i + 1);
 		}
-		
+
+		// start all transactions using a thread pool 
+		ExecutorService pool = Executors.newFixedThreadPool(maxConcurrent);				
+		for (int i = 0; i < trans.length; i++) {
+			pool.execute(trans[i]);
+		}		
+		pool.shutdown(); // end program after all transactions are done
 	}
 }
  
--- a6/code/code.sh
+++ a6/code/code.sh
@@ -1,2 +1,3 @@
+cd fill_tables; ./code.sh; cd -
 javac -cp ".:postgresql-9.4.1211.jar" ConcurrentTransactions.java
 java -cp ".:postgresql-9.4.1211.jar" ConcurrentTransactions $1 $2
